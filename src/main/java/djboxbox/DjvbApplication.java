46e65ee3013f9eaacd2bfa3138c1bab3f0b3478d
twjordan <twjordan@gmail.com>
2015-08-03 18:49:21 -0700

    initial

---
+++ src/main/java/djboxbox/DjvbApplication.java
@@ -0,0 +1,12 @@
+package djboxbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DjvbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DjvbApplication.class, args);
+    }
+}
