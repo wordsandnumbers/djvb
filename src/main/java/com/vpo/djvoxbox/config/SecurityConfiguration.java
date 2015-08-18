85ac18d445e8e980acd8f8df10b3e4838fb1f52c
twjordan <twjordan@gmail.com>
2015-08-17 18:17:31 -0700

    CORS and csrf

--- src/main/java/com/vpo/djvoxbox/config/SecurityConfiguration.java
+++ src/main/java/com/vpo/djvoxbox/config/SecurityConfiguration.java
@@ -25,7 +25,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-        	.csrf().disable()
             .authorizeRequests()
             	.antMatchers("/login/**").permitAll()
                 .anyRequest().authenticated()
