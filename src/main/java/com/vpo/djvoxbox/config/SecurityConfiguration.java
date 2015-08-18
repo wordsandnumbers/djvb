18f50694a7d7f0589c84522b531bd893b66ec3b8
twjordan <twjordan@gmail.com>
2015-08-17 19:16:25 -0700

    CSRF

--- src/main/java/com/vpo/djvoxbox/config/SecurityConfiguration.java
+++ src/main/java/com/vpo/djvoxbox/config/SecurityConfiguration.java
@@ -39,7 +39,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-        	.csrf().disable()
             .authorizeRequests()
             	.antMatchers("/login/**").permitAll()
                 .anyRequest().authenticated()
