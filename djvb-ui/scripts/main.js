c75b52d67c9985d4322ef6d47c673d893a39933d
jsawyer <wordsandnumbers@gmail.com>
2015-12-12 14:04:28 -0800

    Fixed project deps

--- djvb-ui/scripts/main.js
+++ djvb-ui/scripts/main.js
@@ -10,11 +10,7 @@ require.config({
     lodash: '../../bower_components/lodash/lodash',
     digits: 'https://cdn.digits.com/1/sdk',
     'angular-aria': '../../bower_components/angular-aria/angular-aria',
-    'angular-cookies': '../../bower_components/angular-cookies/angular-cookies',
-    'angular-mocks': '../../bower_components/angular-mocks/angular-mocks',
-    'angular-resource': '../../bower_components/angular-resource/angular-resource',
-    'angular-route': '../../bower_components/angular-route/angular-route',
-    'angular-touch': '../../bower_components/angular-touch/angular-touch'
+    'angular-mocks': '../../bower_components/angular-mocks/angular-mocks'
   },
   shim: {
     angular: {
