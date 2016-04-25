42e08af2ebbab96995d6a2054c4d4765e1871c49
jsawyer <wordsandnumbers@gmail.com>
2016-04-24 17:39:03 -0700

    Added different listeners

--- src/main/java/com/vpo/djvoxbox/config/WebSocketConfig.java
+++ src/main/java/com/vpo/djvoxbox/config/WebSocketConfig.java
@@ -9,10 +9,13 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+
+	static final String MESSAGE_PREFIX = "/topic";
+	
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry config) {
 		config.setApplicationDestinationPrefixes("/ws");
-		config.enableSimpleBroker("/topic");
+		config.enableSimpleBroker(MESSAGE_PREFIX);
 	}
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
--- src/main/java/com/vpo/djvoxbox/domain/UserQueueListener.java
+++ src/main/java/com/vpo/djvoxbox/domain/UserQueueListener.java
@@ -2,9 +2,10 @@ package com.vpo.djvoxbox.domain;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.mapping.event.AbstractMongoEventListener;
+import org.springframework.data.mongodb.core.mapping.event.AfterConvertEvent;
+import org.springframework.data.mongodb.core.mapping.event.AfterDeleteEvent;
+import org.springframework.data.mongodb.core.mapping.event.AfterSaveEvent;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-
-import com.mongodb.DBObject;
 
 public class UserQueueListener extends AbstractMongoEventListener<UserQueue> {
 
@@ -12,9 +13,18 @@ public class UserQueueListener extends AbstractMongoEventListener<UserQueue> {
     SimpMessagingTemplate template;
 	
 	@Override
-	public void onAfterSave(UserQueue uq, DBObject dbo) {
-		//template.convertAndSendToUser(((Principal) principal).getName(), "/topic/queue", uq);
-	    template.convertAndSend("/topic/queue/" + uq.getSession().getSession().toString(), uq);
+	public void onAfterConvert(AfterConvertEvent<UserQueue> event) {
+	    template.convertAndSend("/topic/queue/" + event.getSource().getSession().getSession().toString(), event.getSource());
+	}
+
+	@Override
+	public void onAfterSave(AfterSaveEvent<UserQueue> event) {
+	    template.convertAndSend("/topic/queue/update/" + event.getSource().getId(), event.getSource());
+	}
+
+	@Override
+	public void onAfterDelete(AfterDeleteEvent<UserQueue> event) {
+	    template.convertAndSend("/topic/queue/delete/" + event.getDBObject().get("id"), event.getDBObject());
 	}
 
 }
