bad0aac1d02e63cac512e9379a1e05df2b19b9d7
Justin Sawyer <jsawyer@collegenet.com>
2016-07-08 14:17:33 -0700

    Couple more colorpicker things.

--- djvb-ui/scripts/directives/colorinput.js
+++ djvb-ui/scripts/directives/colorinput.js
@@ -17,12 +17,12 @@ define(['angular'], function (angular) {
 				scope: {
 					color: '=ngModel'
 				},
-				template: '<button class="button button-small button-light" title="Change user color" ng-style="{\'background-color\': color}" ng-click="pickColor($event)">Change</button>'
+				template: '<button class="button button-small button-light" title="Selected color: {{color}}. Change user color" ng-style="{\'background-color\': color}" ng-click="pickColor($event)">Change</button>'
 			};
 			
 			function ColorInputLink (scope, element, attrs, modelCtrl) {
 
-				scope.userColors = ['#72C2FF', '#6798E6', '#CEA1E1', '#FFADED', '#FC809B', '#FFC787', '#FFF074', '#A6FC81', '#09E6AE', '#18C4C7',
+				scope.colorOptions = ['#72C2FF', '#6798E6', '#CEA1E1', '#FFADED', '#FC809B', '#FFC787', '#FFF074', '#A6FC81', '#09E6AE', '#18C4C7',
 				'#247ba0', '#70c1b3', '#b2dbbf', '#f3ffbd', '#ff1654',
 				'#eae8ff', '#d8d5db', '#adacb5', '#b0d7ff', '#8e443d', '#cb9173'];
 
@@ -40,6 +40,10 @@ define(['angular'], function (angular) {
 				scope.pickColor = function ($event) {
 					scope.popover.show($event);
 				};
+				
+				scope.isSelected = function (color) {
+					return color.toLowerCase() === scope.color.toLowerCase();
+				}
 			}
 		});
 });
